bdf570aaa68f7d42a2574628c227c6a835ad9268
Chengziqian <18663104463@163.com>
2018-02-05 14:05:21 +0800

    rename StateBar and rename HingeProducingResourceController

--- Assets/Scripts/Hinge/HingeProducingResourceController.cs
+++ Assets/Scripts/Hinge/HingeProducingResourceController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ProductingResourceController : MonoBehaviour {
+public class HingeProducingResourceController : MonoBehaviour {
 
     private Animator animator;
     private Text ResourceValueUI;
