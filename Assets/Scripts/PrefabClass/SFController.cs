f1df719277c1057830f9122a324d0fe08da372f7
Chengziqian <18663104463@163.com>
2018-02-10 09:47:56 +0800

    add script SFController

--- Assets/Scripts/PrefabClass/SFController.cs
+++ Assets/Scripts/PrefabClass/SFController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoldColor.Config;
 
 public class SFController : MonoBehaviour {
-
+    public GameObject GameBody;
+    public GameObject Info;
+    public GameObject BodyCollider;
+    public GameObject BuffController;
+    public GameObject Interact;
+    public Color Camp;
 	// Use this for initialization
 	void Start () {
-		
+        Camp = CampDefine.Campless;
+        GameBody.GetComponent<SpriteRenderer>().color = Camp;
+        BodyCollider.GetComponent<ColliderController>().Camp = Camp;
 	}
 	
 	// Update is called once per frame
