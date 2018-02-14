ca0dd8c44b7ae7ac16e0666056287880f8d6e727
Chengziqian <18663104463@163.com>
2018-02-14 10:49:19 +0800

    create building system 2

--- Assets/Scripts/Turret/TurretBuildTipController.cs
+++ Assets/Scripts/Turret/TurretBuildTipController.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using HoldColor.Config;
 
 public class TurretBuildTipController : MonoBehaviour {
     private float Radius;
+    private bool IsBuildAbled;
+    public GameObject TurretUIBTN;
 	// Use this for initialization
 	void Start () {
         Radius = TurretConfig._InteractAreaRadius;
         gameObject.GetComponentInChildren<BuildAreaTip>().Radius = Radius;
         gameObject.GetComponent<SpriteRenderer>().color = UI._BuildingDiasbled;
+        IsBuildAbled = false;
+        TurretUIBTN = GameObject.Find("UI/BuildTurret");
 	}
     private void Update()
     {
         Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         target.z = 0;
         gameObject.transform.position = target;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (IsBuildAbled)
+            {
+                Debug.Log("build");
+                TurretUIBTN.GetComponent<Button>().interactable = true;
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Debug.Log("cant");
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("cancle");
+            TurretUIBTN.GetComponent<Button>().interactable = true;
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -24,6 +49,7 @@ public class TurretBuildTipController : MonoBehaviour {
         if (colliderController.Type == ColliderController.ColliderType.FieldCollider)
         {
             gameObject.GetComponent<SpriteRenderer>().color = UI._BuildingDiasbled;
+            IsBuildAbled = false;
         }
     }
 
@@ -33,6 +59,7 @@ public class TurretBuildTipController : MonoBehaviour {
         if (colliderController.Type == ColliderController.ColliderType.FieldCollider)
         {
             gameObject.GetComponent<SpriteRenderer>().color = UI._BuildingAbled;
+            IsBuildAbled = true;
         }
     }
 }
--- Assets/Scripts/UI/TurretButtonController.cs
+++ Assets/Scripts/UI/TurretButtonController.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretButtonController : MonoBehaviour {
+    private Button BTN;
+    private void Start()
+    {
+        BTN = gameObject.GetComponent<Button>();
+        BTN.onClick.AddListener(StartTurretBuildTip);
+    }
 
-	
+    private void StartTurretBuildTip ()
+    {
+        GameObject TBT = Resources.Load<GameObject>("Prefabs/TurretBuildTip");
+        Instantiate(TBT);
+        BTN.interactable = false;
+    }
 }
